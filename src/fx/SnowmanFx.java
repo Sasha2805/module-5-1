3deaed2ac51f1786439d37bfa4ce77b83680032f
Sasha Kaminska <sasha249133@ukr.net>
2017-10-09 21:56:57 +0300

    Класс Snowman

--- src/fx/SnowmanFx.java
+++ src/fx/SnowmanFx.java
@@ -1,17 +1,27 @@
 package fx;
 
-import javafx.scene.paint.Color;
+import javafx.scene.paint.*;
 import javafx.scene.shape.Circle;
-
 import java.util.Random;
 import java.util.Scanner;
 
-public class SnowmanOneFx {
-    public static final int WIDTH = 600;
-    public static final int HEIGHT = 700;
+import static javafx.scene.paint.Color.WHITE;
+
+public class SnowmanFx {
     private int countCircles;
     private double minRadius;
     private double maxRadius;
+
+    // Добавила констукторы
+    public SnowmanFx(){
+        this(0, 0, 0);
+    }
+
+    public SnowmanFx(int countCircles, double minRadius, double maxRadius){
+        this.countCircles = countCircles;
+        this.minRadius = minRadius;
+        this.maxRadius = maxRadius;
+    }
 
     public void countCircles(Scanner in){   // Ввод количества кругов
         System.out.print("Введите количество кругов: ");
@@ -24,6 +34,7 @@ public class SnowmanOneFx {
         System.out.print("Введите максимальный радиус круга: ");
         this.maxRadius = in.nextDouble();
     }
+
     public static Color RandomColor() {  // Генерация цвета круга
         Random random = new Random();
         return Color.color(random.nextDouble(), random.nextDouble(), random.nextDouble());
@@ -35,21 +46,21 @@ public class SnowmanOneFx {
 
     public Circle drawCircle(){    // Рисуем круг
         Circle circle = new Circle(RandomRadius(minRadius, maxRadius));
-        circle.setFill(Color.WHITE);
+        circle.setFill(WHITE);
         circle.setStroke(RandomColor());
         circle.setStrokeWidth(2);
         return circle;
     }
 
-    public Circle[] drawSnowman(){
+    public Circle[] drawSnowman(int widthWindow, int heightWindow){
         Circle[] circles = new Circle[countCircles];   // Создаем массив с кругами
         for (int i = 0; i < circles.length; i++){
             circles[i] = drawCircle();
         }
 
         // Задаем координаты кругам
-        double centerX = WIDTH / 2;
-        double centerY = HEIGHT - circles[0].getRadius() - circles[0].getStrokeWidth();
+        double centerX = heightWindow / 2; // Рисуем по центру окна
+        double centerY = widthWindow - circles[0].getRadius() - circles[0].getStrokeWidth();
         for (int i = 0; i < circles.length; i++){
             if (i == 0) {
                 circles[i].setCenterX(centerX);
@@ -91,4 +102,58 @@ public class SnowmanOneFx {
         }
         return face;
     }
+
+    public Circle[] paintCircles(Color color, int widthWindow, int heightWindow){ // Красим круги в нужный цвет
+        Circle[] snowman = drawSnowman(widthWindow, heightWindow);
+        for (int i = 0; i < snowman.length; i++){
+            snowman[i].setFill(color);
+            snowman[i].setStroke(color);
+        }
+        return snowman;
+    }
+
+    public Circle[] snowmanGradient(int widthWindow, int heightWindow){ // Градиент
+        Circle[] snowman = drawSnowman(widthWindow, heightWindow);
+        Stop[] stops = new Stop[countCircles];
+        int rgb = 30; // Темный серый цвет
+        for (int i = 0; i < stops.length; i++){ // Заполняем массив оттенками светлее
+            stops[i] = new Stop(1,Color.rgb(rgb, rgb, rgb));
+            rgb += 20;
+        }
+
+        for (int i = 0; i < snowman.length; i++){   // Делаем градиент
+            RadialGradient rg = new RadialGradient(290, 5,
+                    5, 5, 7, true,
+                    CycleMethod.NO_CYCLE, stops[i], new Stop(0,Color.WHITE));
+            snowman[i].setFill(rg);
+            snowman[i].setStroke(stops[i].getColor());
+        }
+        return snowman;
+    }
+
+    // Геттеры и сеттеры
+    public void setCountCircles(int countCircles) {
+        this.countCircles = countCircles;
+    }
+
+    public int getCountCircles() {
+        return countCircles;
+    }
+
+    public void setMinRadius(double minRadius) {
+        this.minRadius = minRadius;
+    }
+
+    public double getMinRadius() {
+        return minRadius;
+    }
+
+    public void setMaxRadius(double maxRadius) {
+        this.maxRadius = maxRadius;
+    }
+
+    public double getMaxRadius() {
+        return maxRadius;
+    }
+
 }
